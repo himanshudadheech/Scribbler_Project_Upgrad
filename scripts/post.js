34051afb648ba6c8a2ccab4160881dc2a2275d74
Himanshu Dadheech <19etccs026@technonjr.org>
2022-04-17 01:34:09 +0530

    pre final completed code

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,4 @@
+// For adding comments
 function addComments(id) {
   var addEventName = id.value + "<br>";
   var a = document.getElementById("userComments");
@@ -5,6 +6,7 @@ function addComments(id) {
     "<p>" + addEventName + "</p>";
   a.value = a.defaultValue;
 }
+//  For liked count
 var numberOfClicks = 0;
 function postLiked() {
   document.getElementById("likeBlog").innerHTML =
@@ -18,5 +20,25 @@ function postLiked() {
       document.getElementById("likeCount").innerHTML =
         numberOfClicks + " people have liked this!";
     }
+  }
+}
+//  For edit button to change to save
+
+var editMode = false;
+function postEdit() {
+  const paragraph = document.getElementById("paraText");
+  const title = document.getElementById("titletext");
+  var edit_button = document.getElementById("edit");
+
+  if (editMode == false) {
+    title.contentEditable = true;
+    paragraph.contentEditable = true;
+    edit_button.innerHTML = "Save " + `<i class="fa fa-save"></i>`;
+    editMode = true;
+  } else {
+    title.contentEditable = false;
+    paragraph.contentEditable = false;
+    edit_button.innerHTML = "Edit " + `<i class="fa fa-edit"></i>`;
+    editMode = false;
   }
 }
